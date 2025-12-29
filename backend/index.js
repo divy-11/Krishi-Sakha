b51042398009834bebdd885aa01a19f86a78659e
divy-11 <devvv.sangwan@gmail.com>
2025-12-29 23:47:39 +0530

    removed axios dependency
     unnecessary 'axios' dependency in api

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mainRouter = require("./routes/index");
 const app = express();
 const cors = require("cors");
-const axios = require("axios");
 
 app.use(cors());
 app.use(express.json());
